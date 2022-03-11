234971641cde351c8aa7c64d910199fc2e9f77f6
Michael Schultz <35410468+maschultz@users.noreply.github.com>
2022-03-11 17:23:16 -0500

    testing alerts

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -50,34 +50,49 @@ export default function HomePage() {
 		init();
 	}, []);
 
+	// const minter = async () => {
+	// 	try {
+	// 		const connection = await web3Modal.connect();
+	// 		const provider = new ethers.providers.Web3Provider(connection);
+	// 		const signer = provider.getSigner();
+	// 		const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
+	// 	} catch (error) {
+	// 		openAlert(
+	// 			'error',
+	// 			error.message ? error.message : 'Transaction is failed.'
+	// 		);
+	// 	}
+	// };
+
 	const mint = async () => {
 		try {
 			if (isConnected === true) {
 				const web3Modal = await getWeb3Modal();
-				// alert('const: web3Modal passed');
+				alert('const: web3Modal passed');
 				const connection = await web3Modal.connect();
-				// alert('const: connection passed');
+				alert('const: connection passed');
 				const provider = new ethers.providers.Web3Provider(connection);
-				// alert('const: provider passed');
+				alert('const: provider passed');
 				const signer = provider.getSigner();
-				// alert('const: signer passed');
+				alert('const: signer passed');
 
 				const { chainId } = await provider.getNetwork();
-				// alert('const: chainid passed');
+				alert('const: chainid passed');
 
 				// console.log('chain id: ' + chainId);
 				// const chainId = await ethereum.request({ method: 'eth_chainId' });
 				if (chainId === 1) {
 					const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
-					// alert('const: contract passed');
+					alert('const: contract passed');
 					let transaction = await contract.mint(mintAmount, {
 						value: ethers.utils.parseEther(String(NFT_PRICE * mintAmount)),
 					});
-					// alert('const: transaction passed');
+					alert('const: transaction passed');
+					console.log('mobile triggered');
 					await transaction.wait();
-					// alert('const: await passed');
+					alert('const: await passed');
 					openAlert('success', 'Minted!');
-					// alert('const: we should not get this far');
+					alert('const: we should not get this far');
 				} else {
 					openAlert('warning', 'Please choose Ethereum mainnet.');
 				}
@@ -89,6 +104,7 @@ export default function HomePage() {
 				'error',
 				error.message ? error.message : 'Transaction is failed.'
 			);
+			alert('failed, it might start the loop over from here?');
 		}
 	};
 
